3a6a8f5c341707f675d1daa0fb538ebb7f87f123
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-03-22 09:40:14 -0500

    8kyu

---
+++ 8Kyu/square-argument.js
@@ -0,0 +1,17 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+function square(arg) {
+    // Write the "square"-function here
+    return arg * arg  
+  }
+
+
+/* other solutions
+
+const square = (n) => n * n;
+
+const square = a => a * a;
+
+const square = (n) => Math.pow(n, 2)
+
+*/
