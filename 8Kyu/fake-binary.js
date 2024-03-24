1aba1c698af9bb8a05dc549652d4c4a0090cb8f8
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-03-24 10:48:04 -0500

    new kata

---
+++ 8Kyu/fake-binary.js
@@ -0,0 +1,29 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+    let code = ''
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] < 5) {
+      code += '0'
+    } else
+    code += '1'
+  }
+    return code
+  }
+
+
+/*  other solutions
+
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
+
+function fakeBin(x){
+  return x.replace( /[0-4]/g, "0" ).replace( /[5-9]/g, "1" )
+}
+
+fakeBin = x => x.split('').map(e => e < 5 ? 0 : 1).join('');
+
+*/
