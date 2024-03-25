a0772df704acaba390b85d795b8973d691c22499
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-03-25 08:44:35 -0500

    uppercase

---
+++ 8Kyu/make-upper-case.js
@@ -0,0 +1,15 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
+
+/* other solutions
+
+const makeUpperCase = str => str.toUpperCase();
+
+function makeUpperCase(str) {
+  return str.toLocaleUpperCase();
+}
+
+*/
