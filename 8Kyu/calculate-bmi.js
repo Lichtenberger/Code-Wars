14ac711d5b0c32a5656d21b2c37cf8c0d7231ae9
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-03-31 14:35:06 -0500

    bmi

---
+++ 8Kyu/calculate-bmi.js
@@ -0,0 +1,45 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+    if(weight/(height * height) <= 18.5) {
+      return 'Underweight'
+    } else if(weight/(height * height) <= 25) {
+      return 'Normal'
+    } else if(weight/(height * height) <= 30) {
+      return 'Overweight'
+    }
+    return 'Obese'
+  }
+
+
+/* other solutions
+
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
+
+function bmi(weight, height) {
+  var formula = (weight / Math.pow(height, 2));
+  switch (true) {
+    case formula <=18.5:
+    return 'Underweight';
+    case formula <=25.0:
+    return 'Normal';
+    case formula <=30:
+    return 'Overweight';
+    default:
+    return 'Obese';
+    
+  }
+}
+
+*/
