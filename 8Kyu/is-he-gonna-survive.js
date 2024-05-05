3e53dd06a02024cf53165a89591b29b4cffc5780
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-05-05 17:34:19 -0500

    new kata

---
+++ 8Kyu/is-he-gonna-survive.js
@@ -0,0 +1,27 @@
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+function hero(bullets, dragons){
+    //Get Coding!
+      if(bullets/2 - dragons >= 0) {
+        return true
+      }
+      return false
+    }
+
+
+/* other solutions
+
+function hero(bullets, dragons){
+  return bullets >= dragons * 2
+}
+
+function hero(bullets, dragons){
+//Get Coding!
+return (bullets / 2 >= dragons) ? true : false;
+}
+
+const hero = (bullets, dragons) => bullets >= dragons * 2;
+
+*/
